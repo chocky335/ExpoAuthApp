5d36b883fbc42a48876f92022756955a4abe4781
Bohdan Pomohaibo <b.pomohaibo@globallogic.com>
2022-11-29 03:42:00 +0200

    feat: implement NotAuthorizedScreen

--- screens/NotAuthorized.tsx
+++ screens/NotAuthorized.tsx
@@ -1,15 +1,73 @@
-import { StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { Platform, StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import * as WebBrowser from 'expo-web-browser';
+import * as Facebook from 'expo-auth-session/providers/facebook';
+import * as Google from 'expo-auth-session/providers/google';
+import { ResponseType } from 'expo-auth-session';
+
+import AuthButton from '../components/AuthButton';
+import { View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import env from '../env.json';
+import AuthContext, { selectorError, selectorLoadingProvider } from '../context/AuthService';
+import { MonoText } from '../components/StyledText';
+
+
+WebBrowser.maybeCompleteAuthSession();
+
+
+const googleAuthConfig = {
+  androidClientId: env.GOOGLE_ANDROID_ID,
+  webClientId: env.GOOGLE_WEB_ID,
+}
+const facebookAuthConfig = {
+  clientId: env.FACEBOOK_ID,
+  responseType: ResponseType.Token,
+}
 
 export default function NotAuthorizedScreen({}: RootStackScreenProps<'NotAuthorized'>) {
+  const authContext = useContext(AuthContext)
+  const {authorizeSucceed, authorizeFailed, startOAuth} = authContext
+  const loadingProvider = selectorLoadingProvider(authContext)
+  const isFacebookLoading = loadingProvider === 'facebook'
+  const isGoogleLoading = loadingProvider === 'google'
+  const error = selectorError(authContext)
+
+  React.useEffect(() => {
+    if (Platform.OS !== 'web') WebBrowser.warmUpAsync();
+
+    return () => {
+      if (Platform.OS !== 'web') WebBrowser.coolDownAsync();
+    };
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      {error && <MonoText>Error: {error}</MonoText>}
+      {error && <View style={styles.separator} />}
+      <AuthButton
+        provider="google"
+        authConfig={googleAuthConfig}
+        authHook={Google.useAuthRequest}
+        successCallback={authorizeSucceed}
+        errorCallback={authorizeFailed}
+        startOAuth={startOAuth}
+        isLoading={isGoogleLoading}
+      />
+
+      <View style={styles.separator} />
+
+      <AuthButton
+        provider="facebook"
+        authConfig={facebookAuthConfig}
+        authHook={Facebook.useAuthRequest}
+        successCallback={authorizeSucceed}
+        errorCallback={authorizeFailed}
+        startOAuth={startOAuth}
+        isLoading={isFacebookLoading}
+      />
     </View>
   );
 }
